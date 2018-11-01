23f6eabd33f1c2db65c5be6c8dcee7077223ac0a
MXZTY <mtyso088@mtroyal.ca>
2018-11-01 10:28:47 -0600

    Got app working and added Google Maps API key Working on home screen button functionality

--- app/src/main/java/com/example/mtyso/mru_navigation/MainActivity.java
+++ app/src/main/java/com/example/mtyso/mru_navigation/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.mtyso.mru_navigation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class HomeScreen extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-        //lets gooooo
+
+    }
+
+    protected void startNavScreen(View view){
+        Intent intent = new Intent(this, MapsActivity.class);
 
     }
 }
