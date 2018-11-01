e447df71af9299e16fb776b83d0521a8abb8b520
MXZTY <mtyso088@mtroyal.ca>
2018-11-01 16:03:07 -0600

    Added in Search bar code

--- app/src/main/java/com/example/mtyso/mru_navigation/MapsActivity.java
+++ app/src/main/java/com/example/mtyso/mru_navigation/MapsActivity.java
@@ -13,6 +13,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.SearchView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -124,6 +125,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
             }
         }
+        
+
+
     }
 
 }
--- app/src/test/java/com/example/mtyso/mru_navigation/ExtraTests.java
+++ app/src/test/java/com/example/mtyso/mru_navigation/ExtraTests.java
@@ -1,46 +1,18 @@
 package com.example.mtyso.mru_navigation;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ExtraTests {
 
     //search bar can accept letters and numbers
-
-
-    //search bar takes in user input
     @Test
-    public void bar_AcceptsTextInput() {
+    public void searchBarAcceptsInput(){
+
+        Assert.assertNotNull(SearchView.getInputText());
 
     }
 
-    //search bar returns a result
-    @Test
-    public void bar_returnsResult() {
 
-    }
 
-    //search bar returns location data based on input
-    @Test
-    public void bar_ReturnsLocationDataFromInput() {
-
-    }
-
-    //default location returned when invalid search is made
-    @Test
-    public void bar_DefaultLocationWhenInvalidSearch() {
-
-    }
-
-    //only valid mru hallways codes are accepted
-    @Test
-    public void bar_OnlyAcceptsMRUHallwayCodes() {
-
-    }
-
-    /*
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
-    */
 }
