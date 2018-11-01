b0f5af11dd0f05d98f7c7ade7f1c05a0399d2acd
Marc Francois <mfran540@mtroyal.ca>
2018-11-01 15:29:53 -0600

    Created test files for TDD

---
+++ app/src/test/java/com/example/mtyso/mru_navigation/ExtraTests.java
@@ -0,0 +1,46 @@
+package com.example.mtyso.mru_navigation;
+
+import org.junit.Test;
+
+public class ExtraTests {
+
+    //search bar can accept letters and numbers
+
+
+    //search bar takes in user input
+    @Test
+    public void bar_AcceptsTextInput() {
+
+    }
+
+    //search bar returns a result
+    @Test
+    public void bar_returnsResult() {
+
+    }
+
+    //search bar returns location data based on input
+    @Test
+    public void bar_ReturnsLocationDataFromInput() {
+
+    }
+
+    //default location returned when invalid search is made
+    @Test
+    public void bar_DefaultLocationWhenInvalidSearch() {
+
+    }
+
+    //only valid mru hallways codes are accepted
+    @Test
+    public void bar_OnlyAcceptsMRUHallwayCodes() {
+
+    }
+
+    /*
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+    */
+}
---
+++ app/src/test/java/com/example/mtyso/mru_navigation/TestSearchBarFunctionality.java
@@ -0,0 +1,15 @@
+package com.example.mtyso.mru_navigation;
+
+import junit.framework.AssertionFailedError;
+
+import org.junit.Test;
+
+import static java.util.regex.Pattern.matches;
+import static org.junit.Assert.*;
+
+public class TestSearchBarFunctionality {
+
+
+
+
+}
