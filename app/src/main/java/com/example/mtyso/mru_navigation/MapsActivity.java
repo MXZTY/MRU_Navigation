6d47d0754bc63d5f15b11d7cb3c58a74cc150ef2
Connor Beauchamp <connorbeauchamp@gmail.com>
2018-11-05 17:04:50 -0700

    Test

--- app/src/main/java/com/example/mtyso/mru_navigation/MapsActivity.java
+++ app/src/main/java/com/example/mtyso/mru_navigation/MapsActivity.java
@@ -11,6 +11,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -51,6 +52,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     //widgets
     private EditText mSearchText;
     private ValidateUserInput validate;
+    private BottomNavigationView mMainNav;
 
 
 
